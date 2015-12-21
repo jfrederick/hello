3c477d1e00f7c7354aa8b1d5e523661c5445163e
Jim Frederick <james.p.frederick@gmail.com>
2015-12-21 13:45:36 -0500

    Play with memory usage, strings, converting variable types

--- hello.go
+++ hello.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "strconv"
 
 type Mind struct {
 	name string
@@ -9,6 +10,14 @@ type Mind struct {
 }
 
 func main() {
+	// Initialize and manipulate a pointer to a struct.
     john := Mind{"John", 31, "Artifical Intelligence"}
-    fmt.Println(john.aptitude)
+    john_clone := &john
+    john_clone.aptitude = "Artifical Emotionality"
+    // Examine the memory usage of the Println() function.
+    bytes, _ := fmt.Println(john.aptitude)
+    fmt.Println(bytes)
+    // Toy with strings.
+    strBytes := strconv.Itoa(bytes)
+    fmt.Println("The previous number of bytes allocated was " + strBytes)
 }
